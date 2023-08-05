65d5a4a28a2e50bb40f6504d808fe37740555112
LeonardoVN <gogobobo599@gmail.com>
2023-08-05 18:35:54 -0300

    Ver2

---
+++ TCC2022-master/TheFoods/conexao.php
@@ -0,0 +1,12 @@
+<?php
+    $conn = mysqli_connect("", "", "");
+        mysqli_select_db($conn,"");
+
+    $dsn = 'mysql:host=localhost;port=3306;dbname=radio_noise;';
+    $usuario = 'root';
+    $senha = ''; 
+
+    $dsn = '';
+    $usuario = '';
+    $senha = '';
+?>
